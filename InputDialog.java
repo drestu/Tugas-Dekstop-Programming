ef5461496c63eea47f586df2c340e231dce3c8c7
Dilla Restu Nazula <97388578+drestu@users.noreply.github.com>
2022-03-15 19:51:00 +0700

    Add files via upload

---
+++ InputDialog.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package inputdialog;
+
+/**
+ *
+ * @author LENOVO INDONESIA
+ */
+public class InputDialog {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // open jframe here
+        MyFrame mf = new MyFrame ();
+        mf.show(); //open MyFrame
+    }
+    
+}
